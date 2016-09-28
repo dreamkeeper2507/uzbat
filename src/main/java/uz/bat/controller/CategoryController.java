e028164b75c645cefcf0c70d7a5a16bddaa63d9a
kamil <dreamkeeper2007@mail.ru>
2016-09-28 10:46:31 +0500

    Category fullStack

--- src/main/java/uz/bat/controller/CategoryController.java
+++ src/main/java/uz/bat/controller/CategoryController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import uz.bat.model.entity.Category;
+
 import uz.bat.service.CategoryService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -33,31 +34,67 @@ public class CategoryController
     {
 
         Category category = new Category();
-
-        Long id = Long.valueOf(request.getParameter("id"));
+        Long id = null;
+        if (request.getParameter("id") != null)
+            id = Long.valueOf(request.getParameter("id"));
         if (id != null && id > (long) 0)
         {
             category = categoryService.findOne(id);
         }
         model.addAttribute("category", category);
+        model.addAttribute("categoryList", categoryService.all());
         return "category/category-edit";
     }
 
     @RequestMapping(value = "/category-save", method = RequestMethod.POST)
     public String save(Model model, HttpServletRequest request)
     {
-        Long id = Long.valueOf(request.getParameter("idObject"));
-             return "redirect:/category-view";
+        String categoryName = request.getParameter("categoryName");
+        String categorydescription = request.getParameter("categorydescription");
+        Long categoryId = null;
+        if (request.getParameter("categoryId") != null && !request.getParameter("categoryId").equals(""))
+        {
+            categoryId = Long.valueOf(request.getParameter("categoryId"));
+
+        }
+        logger.info(categoryName + ":" + categorydescription);
+        Category category = null;
+        if (request.getParameter("idObject") != null && !request.getParameter("idObject").equals(""))
+        {
+            Long id = Long.valueOf(request.getParameter("idObject"));
+            if (id != null & id > (long) 0)
+                category = categoryService.findOne(id);
+        } else
+            category = new Category();
+
+        category.setCategoryName(categoryName);
+        category.setCategorydescription(categorydescription);
+        if (categoryId != null && categoryId > (long) 0)
+            category.setCategory(categoryService.findOne(categoryId));
+        else
+            category.setCategory(null);
+        categoryService.create(category);
+        logger.info(category.toString());
+        return "redirect:/category-view";
     }
 
     @RequestMapping(value = "/category-delete", method = RequestMethod.GET)
     public String delete(Model model, HttpServletRequest request)
     {
 
-        Category category = new Category();
+        if (request.getParameter("id") != null )
+        {
+            Long id = Long.valueOf(request.getParameter("id"));
+            try
+            {
+                if (id != null)
+                    categoryService.remove(id);
 
-        Long id = Long.valueOf(request.getParameter("id"));
-
+            } catch (Exception e)
+            {
+                logger.info(e.getMessage());
+            }
+        }
 
         return "redirect:/category-view";
     }
